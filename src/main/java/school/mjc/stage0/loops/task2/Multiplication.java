dd1904421c6262cfc20b38097b7923d97804377a
pavel.ulianov <ulyanovproff@gmail.com>
2024-01-11 16:40:36 +0500

    add some task

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,21 +2,24 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
-        int num = 0;
 
-        if (multiplyByAndToInclusive > 0) {
-            while (num <= multiplyByAndToInclusive) {
-                System.out.println(num * multiplyByAndToInclusive);
-                num++;
+        if (multiplyByAndToInclusive >= 0) {
+            int a = 0;
+            while (a <= multiplyByAndToInclusive) {
+                int result = a * multiplyByAndToInclusive;
+                System.out.println(result);
+                a++;
             }
-
-        } else  {
-            while (num > multiplyByAndToInclusive) {
-                System.out.println(num * multiplyByAndToInclusive);
-                num++;
-
-            }
-
+        } else {
+            
+           // int b = 0;
+         //   while (b >= multiplyByAndToInclusive) {
+          //      int result = b * multiplyByAndToInclusive;
+         //       System.out.println(result);
+         //       b++;
+         //   }
         }
+
+
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -3,5 +3,18 @@ package school.mjc.stage0.loops.task2;
 public class PowerOfTwo {
     public void printPower(int power) {
 
+
+        if (power < 0) {
+            System.out.println("too much power");
+        } else {
+            int a = 2;
+            int currentPower = 0;
+            while (currentPower <= power) {
+                int result = (int) Math.pow(a, currentPower);
+                System.out.println(result);
+                currentPower++;
+            }
+        }
+
     }
 }
